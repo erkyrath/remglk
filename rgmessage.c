f02f905c7094412d55b2da5fe7392c8282f7200b
Andrew Plotkin <erkyrath@eblong.com>
2012-03-28 18:09:17 -0400

    Chop the message line down to a stub.

--- rgmessage.c
+++ rgmessage.c
@@ -10,22 +10,11 @@
 #include "glk.h"
 #include "remglk.h"
 
-/* Nothing fancy here. We store a string, and print it on the bottom line.
-    If pref_messageline is FALSE, none of these functions do anything. */
-
-static char *msgbuf = NULL;
-static int msgbuflen = 0;
-static int msgbuf_size = 0;
+/* ### stubs, will move somewhere else */
 
 void gli_msgline_warning(char *msg)
 {
-    char buf[256];
-    
-    if (!pref_messageline)
-        return;
-        
-    sprintf(buf, "Glk library error: %s", msg);
-    gli_msgline(buf);
+    fprintf(stderr, "Glk library error: %s\n", msg);
 }
 
 void gli_msgline_error(char *msg)
@@ -34,34 +23,3 @@ void gli_msgline_error(char *msg)
 
     exit(1); /* ### or something */
 }
-
-/* ### get rid of this */
-void gli_msgline(char *msg)
-{
-    int len;
-    
-    if (!pref_messageline)
-        return;
-        
-    if (!msg) 
-        msg = "";
-    
-    len = strlen(msg);
-    if (!msgbuf) {
-        msgbuf_size = len+80;
-        msgbuf = (char *)malloc(msgbuf_size * sizeof(char));
-    }
-    else if (len+1 > msgbuf_size) {
-        while (len+1 > msgbuf_size)
-            msgbuf_size *= 2;
-        msgbuf = (char *)realloc(msgbuf, msgbuf_size * sizeof(char));
-    }
-
-    if (!msgbuf)
-        return;
-    
-    strcpy(msgbuf, msg);
-    msgbuflen = len;
-    
-}
-
