455a88de5873e305d41df8500d95ec6a9d9beded
Andrew Plotkin <zarf@Zarfslab.local>
2017-02-13 18:04:02 -0500

    Copyright dates on gi_dispa.

--- gi_dispa.h
+++ gi_dispa.h
@@ -5,7 +5,7 @@
     Designed by Andrew Plotkin <erkyrath@eblong.com>
     http://eblong.com/zarf/glk/index.html
 
-    This file is copyright 1998-2012 by Andrew Plotkin. It is
+    This file is copyright 1998-2017 by Andrew Plotkin. It is
     distributed under the MIT license; see the "LICENSE" file.
 */
 
