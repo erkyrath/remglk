6f231cafc640a0ad6406ca3be49cad381c9e12bf
Andrew Plotkin <zarf@ZarfLent.local>
2020-04-19 00:55:43 -0400

    More definitions.

--- glkstart.h
+++ glkstart.h
@@ -11,6 +11,8 @@
     
     Because Glk is *almost* perfectly portable, this interface *almost*
     doesn't have to exist. In practice, it's small.
+
+    (Except for autosave. That makes everything complicated.)
 */
 
 #ifndef GT_START_H
@@ -47,19 +49,33 @@ typedef struct glkunix_startup_struct {
 
 extern glkunix_argumentlist_t glkunix_arguments[];
 
+/* defined in unixstrt.c */
 extern int glkunix_startup_code(glkunix_startup_t *data);
-/* The autosave functions are not defined in every game/interpreter, but
-   if they are defined, they look like this: */
-extern void glkunix_do_autosave(glui32 eventaddr);
-extern glui32 glkunix_window_get_updatetag(winid_t win);
-extern winid_t glkunix_window_find_by_updatetag(glui32 tag);
 
 extern void glkunix_set_base_file(char *filename);
 extern strid_t glkunix_stream_open_pathname_gen(char *pathname, 
     glui32 writemode, glui32 textmode, glui32 rock);
 extern strid_t glkunix_stream_open_pathname(char *pathname, glui32 textmode, 
     glui32 rock);
+
+/* This library offers the hooks necessary for an interpreter to
+   implement autosave. */
+#define GLKUNIX_AUTOSAVE_FEATURES
+
+#ifdef GLKUNIX_AUTOSAVE_FEATURES
+
+/* defined in unixstrt.c */
+extern void glkunix_do_autosave(glui32 eventaddr);
+
 extern glui32 glkunix_get_last_event_type(void);
+extern glui32 glkunix_window_get_updatetag(winid_t win);
+extern winid_t glkunix_window_find_by_updatetag(glui32 tag);
+extern glui32 glkunix_stream_get_updatetag(strid_t str);
+extern strid_t glkunix_stream_find_by_updatetag(glui32 tag);
+extern glui32 glkunix_fileref_get_updatetag(frefid_t fref);
+extern frefid_t glkunix_fileref_find_by_updatetag(glui32 tag);
+
+#endif /* GLKUNIX_AUTOSAVE_FEATURES */
 
 #endif /* GT_START_H */
 
