1bd8ca9c33ed0a4ba513e9f47ceb59622ece1f20
Andrew Plotkin <zarf@ZarfLent.local>
2020-07-12 23:32:46 -0400

    Add a library-level function to unload the blorb map from memory.

--- gi_blorb.h
+++ gi_blorb.h
@@ -2,7 +2,7 @@
 #define _GI_BLORB_H
 
 /* gi_blorb.h: Blorb library layer for Glk API.
-    gi_blorb version 1.6.1.
+    gi_blorb version 1.6.2.
     Designed by Andrew Plotkin <erkyrath@eblong.com>
     http://eblong.com/zarf/glk/
 
@@ -93,6 +93,7 @@ extern giblorb_err_t giblorb_load_image_info(giblorb_map_t *map,
     are necessarily implemented in platform-dependent code. 
 */
 extern giblorb_err_t giblorb_set_resource_map(strid_t file);
+extern giblorb_err_t giblorb_unset_resource_map(void);
 extern giblorb_map_t *giblorb_get_resource_map(void);
 
 #endif /* _GI_BLORB_H */
