6505eaf8d7c39296450d4f50a85b8f14d0b4da11
Andrew Plotkin <zarf@ZarfLent.local>
2022-07-02 21:09:35 -0400

    More int stuff.

--- rgwin_graph.c
+++ rgwin_graph.c
@@ -94,13 +94,13 @@ void win_graphics_rearrange(window_t *win, grect_t *box, data_metrics_t *metrics
     window_graphics_t *dwin = win->data;
     dwin->owner->bbox = *box;
 
-    double width = box->right - box->left;
-    double height = box->bottom - box->top;
+    int width = box->right - box->left;
+    int height = box->bottom - box->top;
 
-    dwin->graphwidth = (int) (width - metrics->graphicsmarginx);
+    dwin->graphwidth = (int) floor(width - metrics->graphicsmarginx);
     if (dwin->graphwidth < 0)
         dwin->graphwidth = 0;
-    dwin->graphheight = (int) (height - metrics->graphicsmarginy);
+    dwin->graphheight = (int) floor(height - metrics->graphicsmarginy);
     if (dwin->graphheight < 0)
         dwin->graphheight = 0;
 }
